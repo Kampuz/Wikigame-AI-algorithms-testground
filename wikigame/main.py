69504c161d0b27532c2ed92ab9155854b11d5b31
Miguel Campos <miguel.moret@unesp.br>
2025-08-17 14:47:36 -0300

    fix: using file instead of folder

--- wikigame/main.py
+++ wikigame/main.py
@@ -1,4 +1,4 @@
-import algoritmos as algo
+from algorithms import *
 
 if __name__ == "__main__":
     start_article = input("Start article: ").capitalize()
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     max_depth = int(input("Max depth: "))
 
     if search_type.upper() == "BFS":
-        path = algo.bfs(start_article, target_article, max_depth)
+        path = bfs.bfs(start_article, target_article, max_depth)
     elif search_type.upper() == "DFS":
-        path = algo.dfs(start_article, target_article, max_depth)
+        path = dfs(start_article, target_article, max_depth)
     elif search_type.upper() == "DLS":
-        path = algo.dls(start_article, target_article, max_depth)
+        path = dls(start_article, target_article, max_depth)
     elif search_type.upper() == "IDS":
-        path = algo.ids(start_article, target_article, max_depth)
+        path = ids(start_article, target_article, max_depth)
     elif search_type.upper() == "UCS":
-        path = algo.ucs(start_article, target_article, max_depth)
+        path = ucs(start_article, target_article, max_depth)
     else:
         print("Opção Inválida.")
